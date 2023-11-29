421a44f07d1e0edb26bebe02da90636b8b0f0f4e
Romain Le Neindre <romain.le.neindre@gmail.com>
2023-11-29 20:14:13 +0100

    Affichage squad show

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -2,6 +2,6 @@ class SquadsController < ApplicationController
   def show
     @matches = Match.all
     @squad = Squad.find(params[:id])
-    @squad_user_bets = @squad.users.map(&:bets).flatten
+    
   end
 end
