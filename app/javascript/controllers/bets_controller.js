a1afe6a82f84cda261b16d2eed53a73ae08a7eed
Romain Le Neindre <romain.le.neindre@gmail.com>
2023-12-01 16:47:58 +0100

    mise en place stimulus

--- app/javascript/controllers/bets_controller.js
+++ app/javascript/controllers/bets_controller.js
@@ -3,9 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="bets"
 export default class extends Controller {
 
+  static targets = ["reward"]
+
   connect() {
 
   }
 
-  
+  RewardSelect(event) {
+    event.preventDefault()
+    console.log("coucou")
+  }
 }
