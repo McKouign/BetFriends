e376c08a36711f28a7a643bc7ebfe435a5c2536b
Romain Le Neindre <romain.le.neindre@gmail.com>
2023-12-03 20:20:47 +0100

    points-counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   has_many :user_rewards, dependent: :destroy
   has_many :rewards, through: :user_rewards
   has_many :bets
+
+  def bet_won_points
+    self.points_counter += 50
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_182339) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_184409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,7 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_182339) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.text "avatar"
-    t.integer "points_counter"
+    t.integer "points_counter", default: 0
     t.bigint "squad_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
