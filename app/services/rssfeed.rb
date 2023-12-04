509a84b8cded8ced9ffc0be375792b5ea2e5d17b
edouard modolo <edouardtravers@gmail.com>
2023-12-04 23:11:24 +0100

    night session : card bet / à finir

--- app/services/rssfeed.rb
+++ app/services/rssfeed.rb
@@ -1,10 +1,14 @@
-class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+class Rssfeed
 
+  def initialize
+    @feed = feed
+    @items = items
+  end
 
-  def home
+  def rssfeed
     url = 'https://rmcsport.bfmtv.com/rss/football/'
     @feed = RSS::Parser.parse(URI.open(url))
   end
+
 
 end
