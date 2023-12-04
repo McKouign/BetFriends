3140d3013e7aff34f3b0708173ef9bf4ed8d9423
edouard modolo <edouardtravers@gmail.com>
2023-12-04 14:06:59 +0100

    implémentation Actualités + refonte UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get 'home', to: 'pages#home'
+
+
   resources :bets, only: %i[create update destroy]
   resources :rewards, only: %i[index] do
     resources :user_rewards, only: %i[create update]
@@ -14,6 +18,8 @@ Rails.application.routes.draw do
   resources :squads, only: %i[show]
   resources :users, only: %i[show]
   resources :matches, only: %i[index]
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
