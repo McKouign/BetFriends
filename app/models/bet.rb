30c452e8d2cfdecdee994d1dedf37f0cd40135a6
Romain Le Neindre <romain.le.neindre@gmail.com>
2023-12-06 20:36:21 +0100

    calcul points

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -9,12 +9,23 @@ class Bet < ApplicationRecord
   def won_bet
     if (match.winner_team == self.participation.try(:id)) || (self.participation.nil? && self.match.draw?)
       self.won!
-        if self.match.funbet == true
+      case
+        when self.match.funbet == true
           self.user.funbet_won_points
+        when self.match.funbet == false && self.user_reward.present?
+          if self.user_reward.reward.title.include? "Plat du pied"
+            self.user.plat_du_pied
+          elsif self.user_reward.reward.title.include? "Poteau rentrant"
+            self.user.poteau_rentrant
+          elsif self.user_reward.reward.title.include? "Panenka!"
+            self.user.panenka
+          else self.user_reward.reward.title.include? "Coup du chapeau"
+            self.user.coup_du_chapeau
+          end
         else
           self.user.won_points
-        end
-      self.save
+      end
+
     end
   end
 end
